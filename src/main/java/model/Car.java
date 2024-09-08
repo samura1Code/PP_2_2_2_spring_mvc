5574f99ed73f72b3170ac3d5e49803c52e0e72d2
Valeriy Makurin <mr.makurin@icloud.com>
2024-09-08 23:28:36 +0300

    implemented car controller with count parameter support
    - Added controller mapped to /cars to display car list
    - Created Car model with three fields: make, model, year
    - Created a list of 5 cars
    - Implemented service to return a specified number of cars
    - Added cars.html page with a table displaying cars using Thymeleaf
    - Added support for count parameter to display a limited number of cars

---
+++ src/main/java/model/Car.java
@@ -0,0 +1,42 @@
+package model;
+
+public class Car {
+
+    private String model;
+    private String color;
+    private int engine;
+
+
+    public Car(){};
+
+    public Car(String model, String color, int engine) {
+        this.model = model;
+        this.color = color;
+        this.engine = engine;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getEngine() {
+        return engine;
+    }
+
+    public void setEngine(int engine) {
+        this.engine = engine;
+    }
+
+}
---
+++ src/main/java/service/CarsService.java
@@ -0,0 +1,9 @@
+package service;
+
+import model.Car;
+
+import java.util.ArrayList;
+
+public interface CarsService {
+   public ArrayList<Car> getAllCars();
+}
